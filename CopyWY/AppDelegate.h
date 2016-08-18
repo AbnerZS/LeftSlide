f92e5eb7819f7ded8633da320694ae3e9b3ac9db
abnerzhang <abnerzhang@abnerzhangdeimac.local>
2016-08-18 15:22:37 +0800

    Initial Commit

---
+++ CopyWY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CopyWY
+//
+//  Created by abnerzhang on 16/8/18.
+//  Copyright © 2016年 abnerzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
